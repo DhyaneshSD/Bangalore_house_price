efcbadcac75ff4dbc1d86d997fb6dd7f73cdb976
DhyaneshSD <74778218+DhyaneshSD@users.noreply.github.com>
2024-02-21 19:31:50 +0530

    update 21/02/24

--- server/util.py
+++ server/util.py
@@ -2,19 +2,13 @@ import json
 import pickle
 import numpy as np
 
-
-__locations =None
+__locations = None
 __data_columns = None
 __model = None
 
-def get_location_names():
-    return __locations
-
-def get_data_columns():
-    return __data_columns
 
 
-def get_estimated_price(location,sqft,bhk,bath):
+def get_estimated_price(location, sqft, bhk, bath):
     global __model
     try:
         loc_index = __data_columns.index(location.lower())
@@ -25,26 +19,40 @@ def get_estimated_price(location,sqft,bhk,bath):
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
-    if loc_index >=0:
+    if loc_index >= 0:
         x[loc_index] = 1
-    return round(__model.predict([x])[0],2)
+    return round(__model.predict([x])[0], 2)
+
 
 def load_saved_artifacts():
     print("Loading saved artifacts...starting")
-    global __data_columns 
+    global __data_columns
     global __locations
     global __model
 
+    try:
+        with open('./artifacts/columns.json', 'r') as f:
+            __data_columns = json.load(f)['data_columns']
+            __locations = __data_columns[3:]
 
-    with open('./artifacts/columns.json','r') as f:
-        __data_columns = json.load(f)['data_columns']
-        __locations = __data_columns[3:]
+        with open("./artifacts/banglore_h_prices_model.pickle", 'rb') as f:
+            __model = pickle.load(f)
 
-    with open("./artifacts/banglore_h_prices_model.pickle",'rb') as f :
-        __model = pickle.load(f)
-    print("Loading saved artifacts....Done")
+        print("Loading saved artifacts....Done")
+
+    except Exception as e:
+        print(f"Error loading artifacts: {str(e)}")
+
+
+def get_location_names():
+    return __locations
+
+
+def get_data_columns():
+    return __data_columns
+
 
 if __name__ == "__main__":
     load_saved_artifacts()
     print(get_location_names())
-    print(get_estimated_price('1st phase jp nagar',1000,3,3))
+    print(get_estimated_price('1st phase jp nagar', 1000, 3, 3))
